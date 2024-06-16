c564819ccaeb8ef88b4ebf14e07c279fc8daae23
Sebastian <sebastianmb88@gmail.com>
2024-06-15 23:21:11 -0500

    Navigate component finalizado

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './store'
 import HomeScreen from './screens/HomeScreen';
@@ -16,34 +16,29 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <Stack.Navigator>
-            <Stack.Screen
-              name='HomeScreen'
-              component={HomeScreen}
-              options={{
-                headerShown:false,
-              }}
-            />
-            <Stack.Screen
-              name='MapScreen'
-              component={MapScreen}
-              options={{
-                headerShown:false,
-              }}
-            />
-          </Stack.Navigator>
-          {/*<HomeScreen />*/}
+          <KeyboardAvoidingView style={{flex:1}}>
+            <Stack.Navigator>
+              <Stack.Screen
+                name='HomeScreen'
+                component={HomeScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name='MapScreen'
+                component={MapScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+            </Stack.Navigator>
+            {/*<HomeScreen />*/}
+          </KeyboardAvoidingView>
+
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { StyleSheet, Text, View, SafeAreaView, TextInput } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableOpacity } from "react-native";
 import tw from "twrnc"
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete"
 import { GOOGLE_MAPS_APIKEY } from "@env"
@@ -7,6 +7,7 @@ import { useDispatch } from "react-redux";
 import { setDestination } from "../slices/navSlice";
 import { useNavigation } from "@react-navigation/native";
 import NavFavorites from "./NavFavorites";
+import { Icon } from "@rneui/base";
 
 
 
@@ -44,6 +45,19 @@ const NavigateCard = () => {
                     />
                 </View>
                 <NavFavorites/>
+            </View>
+
+            <View style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
+                <TouchableOpacity 
+                onPress={()=> navigation.navigate("RideOptionsCard")}
+                style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}>
+                    <Icon name="car" type="font-awesome" color="white" size={16}/>
+                    <Text style={tw`text-white text-center`}>Rides</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}>
+                    <Icon name="fast-food-outline" type="ionicon" color="black" size={16}/>
+                    <Text style={tw`text-center`}>Eats</Text>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     );
